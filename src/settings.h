5aa7974ba84ad9462138ae571c839606c15ee8e1
Christopher Eby <kreed@kreed.org>
2009-03-14 13:31:37 -0500

    Ooops. Fix header guard

--- src/settings.h
+++ src/settings.h
@@ -16,8 +16,8 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef CONFIG_H
-#define CONFIG_H
+#ifndef SETTINGS_H
+#define SETTINGS_H
 
 class QByteArray;
 template<class K, class T> class QHash;
